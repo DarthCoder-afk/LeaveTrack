941b8b54c0f503860a49a8b7d9ae922486de4793
DarthCoder-afk <seanmichaelborje179@gmail.com>
2025-02-07 20:24:48 +0800

    fixed table

--- applications.php
+++ applications.php
@@ -180,9 +180,6 @@ include 'auth.php'; // Ensure authentication
             <!-- DataTable with Hover -->
             <div class="col-lg-12">
               <div class="card mb-4">
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                  <h6 class="m-0 font-weight-bold text-primary">DataTables with Hover</h6>
-                </div>
                 <div class="table-responsive p-3">
                   <table class="table align-items-center table-flush table-hover" id="dataTableHover">
                     <thead class="thead-light">
@@ -195,16 +192,6 @@ include 'auth.php'; // Ensure authentication
                         <th>Salary</th>
                       </tr>
                     </thead>
-                    <tfoot>
-                      <tr>
-                        <th>Name</th>
-                        <th>Position</th>
-                        <th>Office</th>
-                        <th>Age</th>
-                        <th>Start date</th>
-                        <th>Salary</th>
-                      </tr>
-                    </tfoot>
                     <tbody>
                       <tr>
                         <td>Tiger Nixon</td>
