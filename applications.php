bb11bc2399cf3551c8b0650c8233eb6f10e5a217
TeruhashiN <bryllejheraldo@gmail.com>
2025-02-11 10:07:51 +0800

    test

--- applications.php
+++ applications.php
@@ -109,7 +109,7 @@ include 'auth.php'; // Ensure authentication
                 </form>
               </div>
             </li>
-           
+
 
 
             <div class="topbar-divider d-none d-sm-block"></div>
@@ -124,197 +124,197 @@ include 'auth.php'; // Ensure authentication
         </nav>
         <!-- Topbar -->
 
-       
+
         <!-- Container Fluid-->
         <div class="container-fluid" id="container-wrapper">
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Leave Applications</h1>
             <ol class="breadcrumb">
               <li class="breadcrumb-item"><a href="./">Home</a></li>
-              <li class="breadcrumb-item active" aria-current="page">Leave Applications</li>
+              <li class="breadcrumb-item active" aria-current="page">Leave Application</li>
             </ol>
           </div>
 
           <div class="container-fluid">
-          <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800"></h1>
-            <button class="btn btn-primary" data-toggle="modal" data-target="#addApplicationModal">+ ADD</button>
-          </div>
+            <div class="d-sm-flex align-items-center justify-content-between mb-4">
+              <h1 class="h3 mb-0 text-gray-800"></h1>
+              <button class="btn btn-primary" data-toggle="modal" data-target="#addApplicationModal">+ ADD</button>
+            </div>
 
-          <!-- ADD APPLICATION MODAL -->
-          <div class="modal fade" id="addApplicationModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <!-- ADD APPLICATION MODAL -->
+            <div class="modal fade" id="addApplicationModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
               <div class="modal-dialog modal-lg" role="document"> <!-- Increased modal width -->
-                  <div class="modal-content">
-                      <div class="modal-header">
-                          <h5 class="modal-title">Add Leave Application</h5>
-                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                              <span aria-hidden="true">&times;</span>
-                          </button>
-                      </div>
-                      <div class="modal-body">
-                          <form id="addApplicationForm">
-                              <div class="row">
-                                  <div class="col-md-6">
-                                      <div class="form-group">
-                                          <label>Last Name</label>
-                                          <input type="text" class="form-control" id="lastName" required>
-                                      </div>
-                                  </div>
-                                  <div class="col-md-6">
-                                      <div class="form-group">
-                                          <label>First Name</label>
-                                          <input type="text" class="form-control" id="firstName" required>
-                                      </div>
-                                  </div>
-                                  <div class="col-md-6">
-                                      <div class="form-group">
-                                          <label>Middle Name <small>(optional)</small></label>
-                                          <input type="text" class="form-control" id="middleName">
-                                      </div>
-                                  </div>
-                                  <div class="col-md-6">
-                                      <div class="form-group">
-                                          <label>Name Extension <small>(e.g., Jr., Sr., III) (optional)</small></label>
-                                          <input type="text" class="form-control" id="nameExtension">
-                                      </div>
-                                  </div>
-                                  <div class="col-md-6">
-                                      <div class="form-group">
-                                          <label>Position</label>
-                                          <input type="text" class="form-control" id="position" required>
-                                      </div>
-                                  </div>
-                                  <div class="col-md-6">
-                                      <div class="form-group">
-                                          <label>Office</label>
-                                          <input type="text" class="form-control" id="office" required>
-                                      </div>
-                                  </div>
-                                  <div class="col-md-6">
-                                      <div class="form-group">
-                                          <label>Type of Leave</label>
-                                          <input type="text" class="form-control" id="typeOfLeave" required>
-                                      </div>
-                                  </div>
-                                  <div class="col-md-6">
-                                      <div class="form-group">
-                                          <label>Date Applied</label>
-                                          <input type="date" class="form-control" id="dateApplied" required>
-                                      </div>
-                                  </div>
-                                  <div class="col-md-6">
-                                      <div class="form-group">
-                                      <label for="formFile" class="form-label">Upload Scanned Document</label>
-                                      <input type="file" class="form-control" id="formFile">
-                                      </div>
-                                      </div>
-                                 </div>
-                              <button type="submit" class="btn btn-success btn-block">Save</button>
-                          </form>
-                      </div>
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title">Add Leave Application</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
                   </div>
-              </div>
-          </div>
-
-            <!-- Row -->
-            <div class="row">
-            <!-- DataTable with Hover -->
-            <div class="col-lg-12">
-              <div class="card mb-4">
-                <div class="table-responsive p-4">
-                  <table class="table align-items-center table-hover" id="dataTableHover">
-                    <thead class="thead-light text-center">
-                      <tr>
-                        <th>Name</th>
-                        <th>Position</th>
-                        <th>Office</th>
-                        <th>Type of Leave</th>
-                        <th>Date Applied</th>
-                        <th>Actions</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr class="text-center">
-                        <td>John Doe</td>
-                        <td>Job Order</td>
-                        <td>Treasury Office</td>
-                        <td>Sick Leave</td>
-                        <td>2025/02/10</td>
-                        <td>
-                          <button class="btn btn-success"><i class="fas fa-eye"></i></button>
-                          <button class="btn btn-info"><i class="fas fa-pencil-alt"></i></button>
-                          <button class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
-                        </td>
-                      </tr> 
-                    </tbody>
-                  </table>
+                  <div class="modal-body">
+                    <form id="addApplicationForm">
+                      <div class="row">
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label>Last Name</label>
+                            <input type="text" class="form-control" id="lastName" required>
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label>First Name</label>
+                            <input type="text" class="form-control" id="firstName" required>
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label>Middle Name <small>(optional)</small></label>
+                            <input type="text" class="form-control" id="middleName">
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label>Name Extension <small>(e.g., Jr., Sr., III) (optional)</small></label>
+                            <input type="text" class="form-control" id="nameExtension">
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label>Position</label>
+                            <input type="text" class="form-control" id="position" required>
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label>Office</label>
+                            <input type="text" class="form-control" id="office" required>
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label>Type of Leave</label>
+                            <input type="text" class="form-control" id="typeOfLeave" required>
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label>Date Applied</label>
+                            <input type="date" class="form-control" id="dateApplied" required>
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <label for="formFile" class="form-label">Upload Scanned Document</label>
+                            <input type="file" class="form-control" id="formFile">
+                          </div>
+                        </div>
+                      </div>
+                      <button type="submit" class="btn btn-success btn-block">Save</button>
+                    </form>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <!--Row-->
 
-         
-        <!---Container Fluid-->
-    </div>
-
-          
-        <!-- Footer -->
-        <footer class="sticky-footer bg-white">
-          <div class="container my-auto">
-            <div class="copyright text-center my-auto">
-              <span>copyright &copy; <script>
-                  document.write(new Date().getFullYear());
-                </script>
-              </span>
+            <!-- Row -->
+            <div class="row">
+              <!-- DataTable with Hover -->
+              <div class="col-lg-12">
+                <div class="card mb-4">
+                  <div class="table-responsive p-4">
+                    <table class="table align-items-center table-hover" id="dataTableHover">
+                      <thead class="thead-light text-center">
+                        <tr>
+                          <th>Name</th>
+                          <th>Position</th>
+                          <th>Office</th>
+                          <th>Type of Leave</th>
+                          <th>Date Applied</th>
+                          <th>Actions</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr class="text-center">
+                          <td>John Doe</td>
+                          <td>Job Order</td>
+                          <td>Treasury Office</td>
+                          <td>Sick Leave</td>
+                          <td>2025/02/10</td>
+                          <td>
+                            <button class="btn btn-success"><i class="fas fa-eye"></i></button>
+                            <button class="btn btn-info"><i class="fas fa-pencil-alt"></i></button>
+                            <button class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
             </div>
+            <!--Row-->
+
+
+            <!---Container Fluid-->
           </div>
-        </footer>
-        <!-- Footer -->
+
+
+          <!-- Footer -->
+          <footer class="sticky-footer bg-white">
+            <div class="container my-auto">
+              <div class="copyright text-center my-auto">
+                <span>copyright &copy; <script>
+                    document.write(new Date().getFullYear());
+                  </script>
+                </span>
+              </div>
+            </div>
+          </footer>
+          <!-- Footer -->
+        </div>
       </div>
-    </div>
 
-    <!-- Scroll to top -->
-    <a class="scroll-to-top rounded" href="#page-top">
-      <i class="fas fa-angle-up"></i>
-    </a>
+      <!-- Scroll to top -->
+      <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+      </a>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="js/ruang-admin.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+      <script src="vendor/jquery/jquery.min.js"></script>
+      <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+      <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+      <script src="js/ruang-admin.min.js"></script>
       <!-- Page level plugins -->
-    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+      <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+      <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
-    <!-- Page level custom scripts -->
-    <script>
-        $(document).ready(function () {
-        $('#dataTable').DataTable(); // ID From dataTable 
-        $('#dataTableHover').DataTable(); // ID From dataTable with Hover
+      <!-- Page level custom scripts -->
+      <script>
+        $(document).ready(function() {
+          $('#dataTable').DataTable(); // ID From dataTable 
+          $('#dataTableHover').DataTable(); // ID From dataTable with Hover
         });
-    </script>
-    <script>
-      document.getElementById("logout-link").addEventListener("click", function(event) {
-        event.preventDefault(); // Prevent immediate redirection
+      </script>
+      <script>
+        document.getElementById("logout-link").addEventListener("click", function(event) {
+          event.preventDefault(); // Prevent immediate redirection
 
-        Swal.fire({
-          title: "Are you sure?",
-          text: "You will be logged out!",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#d33",
-          cancelButtonColor: "#3085d6",
-          confirmButtonText: "Yes, logout!"
-        }).then((result) => {
-          if (result.isConfirmed) {
-            window.location.href = "logout.php"; // Redirect if confirmed
-          }
+          Swal.fire({
+            title: "Are you sure?",
+            text: "You will be logged out!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3085d6",
+            confirmButtonText: "Yes, logout!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              window.location.href = "logout.php"; // Redirect if confirmed
+            }
+          });
         });
-      });
-    </script>
+      </script>
 
 </body>
 
