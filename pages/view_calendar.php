8a32325e3b312b74fdcb8d42f3cbfbc229779885
DarthCoder-afk <seanmichaelborje79@gmail.com>
2025-02-18 11:15:49 +0800

    made the sidebar as a component

--- pages/view_calendar.php
+++ pages/view_calendar.php
@@ -25,103 +25,10 @@ include '../auth/auth.php'; // Ensure authentication
 
 <body id="page-top">
   <div id="wrapper">
-    <!-- Sidebar -->
-    <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
-      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="../pages/index.php" style="background-color: #2e8517;">
-        <div class="sidebar-brand-icon">
-          <img src="../img/HR.png" style="border-radius: 50%;">
-        </div>
-        <div class="sidebar-brand-text mx-3">HR Admin</div>
-      </a>
-      <hr class="sidebar-divider my-0">
-      <li class="nav-item">
-        <a class="nav-link" href="../pages/index.php">
-          <i class="fas fa-fw fa-tachometer-alt"></i>
-          <span>Dashboard</span></a>
-      </li>
-      <hr class="sidebar-divider">
-      <div class="sidebar-heading">
-        Features
-      </div>
-      <li class="nav-item active">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseBootstrap"
-          aria-expanded="true" aria-controls="collapseBootstrap">
-          <i class="far fa-fw fa-window-maximize"></i>
-          <span>Applications</span>
-        </a>
-        <div id="collapseBootstrap" class="collapse" aria-labelledby="headingBootstrap" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">View</h6>
-            <a class="collapse-item" href="../pages/applications.php">Leave Applications</a>
-            <a class="collapse-item" href="../pages/travel_order.php">Travel Orders</a>
-            <a class="collapse-item" href="../pages/view_calendar.php">Calendar</a>
-          </div>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="../pages/leave_form.php">
-          <i class="fas fa-print"></i>
-          <span>Leave Form</span>
-        </a>
-      </li>
-      <li class="nav-item">
-      <a class="nav-link" href="../pages/employee_list.php">
-          <i class="fas fa-users"></i>
-          <span>Employee List</span>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="../pages/accountsetting.php">
-          <i class="fas fa-cog"></i>
-          <span>Account Settings</span>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" id="logout-link" href="#">
-          <i class="fas fa-sign-out-alt"></i>
-          <span>Log-out</span>
-        </a>
-      </li>
-      <hr class="sidebar-divider">
-    </ul>
-    <!-- Sidebar -->
-    <div id="content-wrapper" class="d-flex flex-column">
-      <div id="content">
-        <!-- TopBar -->
-        <nav class="navbar navbar-expand navbar-light bg-navbar topbar mb-4 static-top" style="background-color: #2e8517;">
-          <button id="sidebarToggleTop" class="btn btn-link rounded-circle mr-3">
-            <i class="fa fa-bars"></i>
-          </button>
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item dropdown no-arrow">
-              <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-search fa-fw"></i>
-              </a>
-              <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                aria-labelledby="searchDropdown">
-                <form class="navbar-search">
-                  <div class="input-group">
-                    <input type="text" class="form-control bg-light border-1 small" placeholder="What do you want to look for?"
-                      aria-label="Search" aria-describedby="basic-addon2" style="border-color: #2e8517;">
-
-                  </div>
-                </form>
-              </div>
-            </li>
-            
-
-            <div class="topbar-divider d-none d-sm-block"></div>
-            <li class="nav-item dropdown no-arrow">
-              <a class="nav-link dropdown-toggle" href="#" id="#" role="#" data-toggle="#"
-                aria-haspopup="false" aria-expanded="false">
-                <img class="img-profile rounded-circle" src="../img/profile.jpg" style="max-width: 60px">
-                <span class="ml-2 d-none d-lg-inline text-white small">Administrator</span>
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <!-- Topbar -->
+         <!-- Sidebar&Topbar -->
+         <?php include '../includes/sidebar.php'; ?>
+         <!-- Sidebar&Topbar -->
+          
         <!-- Container Fluid-->
         <div class="container-fluid" id="container-wrapper">
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
