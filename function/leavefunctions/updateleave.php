02a232d1497752a10ce8571c1632812e092271cd
DarthCoder-afk <seanmichaelborje79@gmail.com>
2025-02-21 20:49:22 +0800

    fixed the update feature

--- function/leavefunctions/updateleave.php
+++ function/leavefunctions/updateleave.php
@@ -3,7 +3,8 @@ include '../../database/db_connect.php';
 
 if(isset($_POST['UpdateLeave'])) {
     session_start();
-    
+
+    $index_no = $_POST['index_no'];
     $emp_id = $_POST['employee_Id'];
     $leavetype = $_POST['leavetype'];
     $datefiled = $_POST['applieddate'];
@@ -14,14 +15,15 @@ if(isset($_POST['UpdateLeave'])) {
     // File Upload
     $file = $_FILES['form']['name'];
     $file_temp = $_FILES['form']['tmp_name'];
-    $file_destination = "../../uploads/".($file);
+    $unique_file_name = time() . '_' . $file; // Append a timestamp to the file name
+    $file_destination = "../../uploads/".($unique_file_name);
     move_uploaded_file($file_temp, $file_destination);
     
-    echo '<script> console.log("'.$emp_id.'"); </script>';
+    echo '<script> console.log("'.$index_no.'"); </script>';
     //echo '<script> console.log("'.$new_emp_id.'"); </script>';
 
-    $stmt = $conn->prepare("UPDATE leaveapplication SET leavetype = ?, dateapplied = ?, startdate = ?, enddate = ?, numofdays = ?, file = ? WHERE employee_id = ?");
-    $stmt->bind_param("sssssss", $leavetype, $datefiled, $sdate, $edate, $ndays, $file, $emp_id);
+    $stmt = $conn->prepare("UPDATE leaveapplication SET leavetype = ?, dateapplied = ?, startdate = ?, enddate = ?, numofdays = ?, file = ? WHERE index_no = ?");
+    $stmt->bind_param("ssssssi", $leavetype, $datefiled, $sdate, $edate, $ndays, $file, $index_no);
 
     if($stmt->execute()) {
         $_SESSION['message'] = "update";
@@ -30,6 +32,7 @@ if(isset($_POST['UpdateLeave'])) {
     } else {
         $_SESSION['message'] = "error";
         echo '<script> alert("Data Not Saved"); </script>';
+        echo '<script> console.log("Error: '.$stmt->error.'"); </script>';
     }
 
     $stmt->close();
