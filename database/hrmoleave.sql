372868701b70be58e12335d315c9974e54a3e47b
DarthCoder-afk <seanmichaelborje79@gmail.com>
2025-02-21 13:20:47 +0800

    updated database

--- database/hrmoleave.sql
+++ database/hrmoleave.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 19, 2025 at 08:56 AM
+-- Generation Time: Feb 21, 2025 at 06:19 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -45,7 +45,30 @@ CREATE TABLE `employee` (
 INSERT INTO `employee` (`employee_id`, `fname`, `midname`, `lname`, `extname`, `gender`, `position`, `office`) VALUES
 ('001', 'Donovan', 'Abriol', 'Mancenido', '', 'Male', 'Municipal Mayor', 'Office of the Municipal Mayor'),
 ('005', 'Ruena', 'Valles', 'Estrella', '', 'Female', 'Administrative Aide VI', 'Office of the Municipal Mayor'),
-('014', 'Maria Cristina', 'Zantua', 'Arevalo', '', 'Female', 'Municipal Vice Mayor', 'Sangguniang Bayan Office');
+('015', 'Maria Cristina', 'Zantua', 'Arevalo', '', 'Female', 'Municipal Vice Mayor', 'Sangguniang Bayan Office');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leaveapplication`
+--
+
+CREATE TABLE `leaveapplication` (
+  `employee_id` varchar(10) NOT NULL,
+  `leavetype` varchar(50) NOT NULL,
+  `dateapplied` date NOT NULL,
+  `startdate` date NOT NULL,
+  `enddate` date NOT NULL,
+  `numofdays` int(100) NOT NULL,
+  `file` blob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `leaveapplication`
+--
+
+INSERT INTO `leaveapplication` (`employee_id`, `leavetype`, `dateapplied`, `startdate`, `enddate`, `numofdays`, `file`) VALUES
+('001', 'Sick Leave', '2025-02-21', '2025-02-24', '2025-02-25', 2, 0x4c656176655f466f726d2e706466);
 
 -- --------------------------------------------------------
 
