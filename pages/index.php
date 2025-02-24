353e18b7294cee66723812781baeaa2bc1cb4d35
DarthCoder-afk <seanmichaelborje79@gmail.com>
2025-02-24 15:08:19 +0800

    fixed dashboard

--- pages/index.php
+++ pages/index.php
@@ -38,8 +38,9 @@ include '../auth/auth.php'; // Ensure authentication
           </div>
 
           <div class="row mb-3">
-            <!-- Earnings (Monthly) Card Example -->
-            <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+
+            <!-- Leave Application -->
+            <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
               <div class="card h-100">
                 <div class="card-body">
                   <div class="row align-items-center">
@@ -58,8 +59,9 @@ include '../auth/auth.php'; // Ensure authentication
                 </div>
               </div>
             </div>
-            <!-- Earnings (Annual) Card Example -->
-            <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+
+            <!-- Travel Order -->
+            <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
               <div class="card h-100">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
@@ -78,8 +80,9 @@ include '../auth/auth.php'; // Ensure authentication
                 </div>
               </div>
             </div>
-            <!-- New User Card Example -->
-            <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+
+            <!-- Monetization -->
+            <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
               <div class="card h-100">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
@@ -93,6 +96,28 @@ include '../auth/auth.php'; // Ensure authentication
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-money-check-alt fa-2x text-success"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- Employee Number -->
+
+            <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
+              <div class="card h-100">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">No. of Employees</div>
+                      <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">10</div>
+                      <div class="mt-2 mb-0 text-muted text-xs">
+                        <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 20.4%</span>
+                        <span>Since last month</span>
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-users fa-2x text-success"></i>
                     </div>
                   </div>
                 </div>
