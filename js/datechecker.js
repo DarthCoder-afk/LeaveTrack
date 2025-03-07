0ed85e4444360094f3ac374d03bea0def9d5cc87
TeruhashiN <bryllejheraldo@gmail.com>
2025-03-07 18:59:06 +0800

    Date number including Saturday and Sunday

--- js/datechecker.js
+++ js/datechecker.js
@@ -3,14 +3,10 @@ function calculateNumberofDays() {
   const endDate = document.getElementById("endDate").value;
 
   if (startDate && endDate) {
-    const start = new Date(startDate);
-    const end = new Date(endDate);
-    const timeDifference = end - start;
-    const daysDifference = timeDifference / (1000 * 3600 * 24) + 1;
-
-    if (daysDifference > 0) {
-      document.getElementById('numberOfDays').value = daysDifference;
-    } else {
+    let start = new Date(startDate);
+    let end = new Date(endDate);
+    
+    if (end < start) {
       document.getElementById('startDate').value = '';
       document.getElementById('endDate').value = '';
       Swal.fire({
@@ -18,6 +14,20 @@ function calculateNumberofDays() {
         text: "End date should be greater than start date.",
         icon: "error"
       });
+      return;
     }
+
+    let weekdays = 0;
+    let currentDate = new Date(start);
+
+    while (currentDate <= end) {
+      let dayOfWeek = currentDate.getDay();
+      if (dayOfWeek !== 6 && dayOfWeek !== 0) { // Exclude Saturday (6) and Sunday (0)
+        weekdays++;
+      }
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+    document.getElementById('numberOfDays').value = weekdays;
   }
 }
