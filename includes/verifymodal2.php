228ad86743f77421988d3cbcd142a952b5508454
DarthCoder-afk <seanmichaelborje179gmail.com>
2025-04-23 14:42:58 +0800

    fixed icon position

--- includes/verifymodal2.php
+++ includes/verifymodal2.php
@@ -4,7 +4,7 @@
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title text-primary font-weight-bold" id="exampleModalLongTitle">Update this data? 🛠️</h5>
+                <h5 class="modal-title text-primary font-weight-bold" id="exampleModalLongTitle">🛠️ Update this data? </h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
