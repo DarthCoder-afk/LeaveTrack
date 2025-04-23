29b058fdcbf0e1b4f747408677c4d648c5d97f6d
DarthCoder-afk <seanmichaelborje179gmail.com>
2025-04-23 15:06:22 +0800

    made history table as DataTable

--- pages/history.php
+++ pages/history.php
@@ -19,6 +19,7 @@ include '../auth/auth.php'; // Ensure authentication
   <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
   <link href="../css/admin.min.css" rel="stylesheet">
+  <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.css" rel="stylesheet">
 
 
@@ -40,31 +41,32 @@ include '../auth/auth.php'; // Ensure authentication
                 </ol>
             </div>
           
-            <div class="card shadow mb-4 mt-2">
-                <div class="tab-content" id="myTabContent">
-                    <!-- Employee Tab -->
-                    <div class="tab-pane fade show active" id="employee" role="tabpanel" aria-labelledby="employee-tab">
-                        <div class="card">
-                            <div class="table-responsive">
-                            <table class="table align-items-center table-flush text-center" style="table-layout: fixed; width: 100%;">
-                                <thead class="thead-light">
-                                    <tr>
-                                        <th class="text-wrap" style="width: 10%;">No.</th>
-                                        <th class="text-wrap" style="width: 70%;">Action Taken</th>
-                                        <th class="text-wrap" style="width: 20%;">Timestamp</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php include '../function/historyfunction/fetchhistory.php' ?>
-                                </tbody>
-                            </table>
-                            </div>
-                        </div>
-                    </div>
-                   
-                  
+             <!-- Leave Application Table -->
+             <div class="row">
+              <!-- DataTable with Hover -->
+              <div class="col-lg-12">
+                <div class="card mb-4">
+                  <div class="table-responsive p-4">
+                    <table class="table align-items-center table-hover" id="dataTableHover">
+                      <thead class="thead-light text-center">
+                        <tr>
+                          <th>No.</th>
+                          <th>Action Taken</th>
+                          <th>Timestamp</th>
+                          
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <!-- Table Data -->
+                        <?php include '../function/historyfunction/fetchhistory.php'; ?>
+                      </tbody>
+                    </table>
+                  </div>
                 </div>
+              </div>
             </div>
+            <!--Row-->
+
         </div>
     </div>
 
@@ -73,12 +75,14 @@ include '../auth/auth.php'; // Ensure authentication
     <a class="scroll-to-top rounded" href="#page-top">
       <i class="fas fa-angle-up"></i>
     </a>
+
     <!-- FullCalendar JS -->
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js"></script>
      <!-- Clickable Date JS -->
     <script src="calendar.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="../vendor/jquery/jquery.min.js"></script>
+
     <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
     <script src="../js/ruang-admin.min.js"></script>
@@ -86,8 +90,9 @@ include '../auth/auth.php'; // Ensure authentication
     <script src="../js/demo/chart-area-demo.js"></script>
     <script src="../js/logout.js"></script>
 
-    <!-- JavaScript for Drag & Drop -->
-    <script src="../js/draganddrop.js"></script>
+    <script src="../vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="../vendor/datatables/dataTables.bootstrap4.min.js"></script>
+    <script src="../js/dataTable.js"></script>
 
 </body>
 
